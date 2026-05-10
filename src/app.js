5300282efd8b747c85344bf9ff60f1d416993cf4
alkeshmahamune <alkeshmahamune@gmail.com>
2026-05-10 22:51:12 +0530

    added the home route in app

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ app.use('/',schoolRouter)
 // port from .env or default port 
 const PORT = config.port || 5000;
 
+// home route 
+app.get("/", (req, res) => {
+  res.send("School Management API Running");
+});
+
 //server 
 app.listen(PORT,()=>{
     console.log('SERVER IS UP AND RUNNING...'+PORT)
